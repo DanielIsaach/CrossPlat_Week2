ea02cc5df1433a1ceb5edd1b77d49ed20aa382ae
StrawCraft <daniel.isaach@student.umn.ac.id>
2025-02-20 19:18:11 +0700

    1

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView >
+      <View style={styles.container}>
+      <Image source = {require('./assets/sponge.png')} />
+      <Text>Spongebob Squarepants</Text>
+      <Text>0000001</Text>
+      <Image source = {require('./assets/Pat.png')} />
+      <Text>Patrick Star</Text>
+      <Text>0000002</Text>
+      <Image source = {require('./assets/Squid.png')} />
+      <Text>Squidward Tentacle</Text>
+      <Text>0000003</Text>
+      <Image source = {require('./assets/Sandy.png')} />
+      <Text>Sandy Squirell</Text>
+      <Text>0000004</Text>
+      <Image source = {require('./assets/Crab.png')} />
+      <Text>Eugine Crab</Text>
+      <Text>0000005</Text>
+      <Image source = {require('./assets/Plank.png')} />
+      <Text></Text>
       <StatusBar style="auto" />
-    </View>
+      </View>
+    </ScrollView>
   );
 }
 
